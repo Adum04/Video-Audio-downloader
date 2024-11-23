f90768eda7926c932636988181afc1629ec2b8ef
Adum04 <adum0407@gmail.com>
2024-11-23 21:55:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -110,7 +110,7 @@ def download_video(url):
         "format": "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best",
         "outtmpl": filepath,
         "merge_output_format": "mp4",
-        "cookies": "cookies/cookies.txt",
+        "cookies": "path_to_cookies/cookies.txt",
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
